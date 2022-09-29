15d35350ab3944d430383c035803d611aba66ee2
Admin <Admin@DESKTOP-2DFAO40>
2022-09-30 00:16:24 +0600

    Добавьте файлы проекта.

---
+++ HouseDZ/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HouseDZ
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            MyArray array = new MyArray(12);
+            array[7] = 4;
+            Console.WriteLine(array[7]); 
+
+        }
+        class MyArray
+        {
+            int[] arr;
+
+            public MyArray(int size) => arr = new int[size];
+
+            public int this[int index]
+            {
+                get => arr [index];
+                set => arr[index] = value * value;
+            }
+        }
+    }
+}
